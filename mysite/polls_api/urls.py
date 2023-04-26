ca6df4c5d7dc53a75be0c1358014920951061240
kmus1232 <kmus1232@gmail.com>
2023-04-26 13:08:38 +0900

    question-detail GET POST DELETE 구현

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('question/', question_list, name='question_list'),
+    path('question/<int:id>/', question_detail, name='question_detail'),
 ]
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+
 
 @api_view(["GET", "POST"])
 def question_list(request):
@@ -16,6 +18,26 @@ def question_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def question_detail(request, id):
+    question = get_object_or_404(Question, pk=id)
+
+    if request.method == "GET":
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "PUT":
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "DELETE":
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
