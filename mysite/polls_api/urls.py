bc330db0d83e2e2e4f8e80145475511144468049
kmus1232 <kmus1232@gmail.com>
2023-04-26 12:33:54 +0900

    question/ POST 구현

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('questions/', question_list, name='question_list'),
+    path('question/', question_list, name='question_list'),
 ]
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -2,9 +2,20 @@ from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
-@api_view()
+@api_view(["GET", "POST"])
 def question_list(request):
-    questions = Question.objects.all()
-    serializer = QuestionSerializer(questions, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
