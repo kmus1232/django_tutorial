446173de63a86a81e65962f86aeaabd39129dc20
kmus1232 <kmus1232@gmail.com>
2023-04-26 13:50:43 +0900

    Generic API View 적용

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,32 +1,13 @@
-from django.shortcuts import get_object_or_404
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import generics
 
 
-class QuestionList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class QuestionDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)  # RetrieveModelMixin
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)  # UpdateModelMixin
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)  # DestroyModelMixin
